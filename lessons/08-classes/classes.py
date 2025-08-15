048c3e839e4f221fe15e470c30ab3bbb1fa06c6c
Tomasz Szpak <tomszp@gmail.com>
2025-08-15 09:02:32 +0200

    add code

---
+++ lessons/08-classes/classes.py
@@ -0,0 +1,17 @@
+class Car:
+    brand: str
+    model: str
+    is_driving: bool
+
+    def __init__(self, brand: str, model: str) -> None:
+        self.brand = brand
+        self.model = model
+        self.is_driving = False
+
+    def drive(self):
+        print(f'{self.brand}, model {self.model} is DRIVING!')
+
+    def stop_driving(self):
+        self.is_driving = False
+        print(f'{self.brand}, model {self.model} is STOPPED!')
+
---
+++ lessons/08-classes/code.py
@@ -0,0 +1,4 @@
+from classes import Car
+
+print('__name__: ', __name__)
+
